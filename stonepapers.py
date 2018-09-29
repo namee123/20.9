d3945374c0c07d7174e4edc3faa85ded762e96b3
namee123 <43032040+namee123@users.noreply.github.com>
2018-09-29 10:43:35 +0530

    Add files via upload

---
+++ stonepapers.py
@@ -0,0 +1,15 @@
+import random
+print('welcome to the game stone paper scissors')
+g={1:'stone',2:'paper',3:'scissors'}
+
+while True:
+	m=input('your choice stone/paper/scissor:')
+	c=g[random.randint(1,2,3)]
+	print('your choice:',p,'computer choice:',c)
+	if(m=='stone'):
+		print('you win')
+	elif(m=='paper'):
+		if
+		print('its draw')
+	else:
+		print('you win')
